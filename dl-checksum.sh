2004fc562e9cd2f9e92780a585289075cff7924d
Andrew Rothstein <andrew.rothstein@gmail.com>
2017-08-17 23:10:18 -0400

    up to v1.7.4

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env sh
-VER=v1.7.2
+VER=v1.7.4
 DIR=~/Downloads
-wget -O $DIR/kubectl-linux-amd64-$VER https://storage.googleapis.com/kubernetes-release/release/$VER/bin/linux/amd64/kubectl
-wget -O $DIR/kubectl-darwin-amd64-$VER https://storage.googleapis.com/kubernetes-release/release/$VER/bin/darwin/amd64/kubectl
-wget -O $DIR/kubectl-windows-amd64.exe-$VER https://storage.googleapis.com/kubernetes-release/release/$VER/bin/windows/amd64/kubectl.exe
-#sha256sum kubectl-*-$VER
-shasum -a 256 $DIR/kubectl-*-$VER
+MIRROR=https://storage.googleapis.com/kubernetes-release/release
+wget -O $DIR/kubectl-linux-amd64-$VER $MIRROR/$VER/bin/linux/amd64/kubectl
+wget -O $DIR/kubectl-darwin-amd64-$VER $MIRROR/$VER/bin/darwin/amd64/kubectl
+wget -O $DIR/kubectl-windows-amd64.exe-$VER $MIRROR/$VER/bin/windows/amd64/kubectl.exe
+sha256sum kubectl-*-$VER
+#shasum -a 256 $DIR/kubectl-*-$VER
