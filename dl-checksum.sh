31631701f8872c77581b0520fc48f83a4d12c1f1
Andrew Rothstein <andrew.rothstein@gmail.com>
2019-06-24 08:24:24 -0500

    up to v1.15.0

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-VER=v1.14.3
+VER=v1.15.0
 DIR=~/Downloads
 MIRROR=https://storage.googleapis.com/kubernetes-release/release/$VER/bin
 
@@ -8,12 +8,23 @@ dl()
     OS=$1
     PLATFORM=$2
     SUFFIX=${3:-}
-    wget -O $DIR/kubectl-$OS-$PLATFORM-$VER $MIRROR/$OS/$PLATFORM/kubectl$SUFFIX
+    URL=$MIRROR/$OS/$PLATFORM/kubectl$SUFFIX
+    LFILE=$DIR/kubectl-$OS-$PLATFORM-$VER 
+
+    if [ ! -e $LFILE ];
+    then
+        wget -q -O $LFILE $URL
+    fi
+
+    printf "  %s:\n" $OS
+    printf "    # %s\n" $URL
+    printf "    %s: sha256:%s\n" $PLATFORM `sha256sum $LFILE | awk '{print $1}'`
 }
 
+printf "%s:\n" $VER
 dl linux amd64
 dl linux arm64
 dl darwin amd64
 dl windows amd64 .exe
-sha256sum $DIR/kubectl-*-$VER
+
 
