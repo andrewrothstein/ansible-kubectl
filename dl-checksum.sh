96fdf9700dd2cd69f37b703b5e7aac01eb7bff31
Andrew Rothstein <andrew.rothstein@gmail.com>
2019-10-23 16:13:22 -0500

    up to v1.16.2. cleanup checksums.

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env sh
-VER=${1:-v1.16.1}
 DIR=~/Downloads
-MIRROR=https://storage.googleapis.com/kubernetes-release/release/$VER/bin
+MIRROR=https://storage.googleapis.com/kubernetes-release/release
 
 dl()
 {
-    local os=$1
-    local arch=$2
-    local suffix=${3:-}
-    local url=$MIRROR/$os/$arch/kubectl$suffix
-    local lfile=$DIR/kubectl-$os-$arch-$VER 
+    local ver=$1
+    local os=$2
+    local arch=$3
+    local suffix=${4:-}
+    local url=$MIRROR/$ver/bin/$os/$arch/kubectl$suffix
+    local lfile=$DIR/kubectl-$os-$arch-$ver
 
     if [ ! -e $lfile ];
     then
@@ -20,13 +20,16 @@ dl()
     printf "      %s: sha256:%s\n" $arch `sha256sum $lfile | awk '{print $1}'`
 }
 
-printf "  %s:\n" $VER
-printf "    %s:\n" linux
-dl linux amd64
-dl linux arm64
-printf "    %s:\n" darwin
-dl darwin amd64
-printf "    %s:\n" windows
-dl windows amd64 .exe
+dlver () {
+    local ver=$1
+    printf "  %s:\n" $ver
+    printf "    %s:\n" linux
+    dl $ver linux amd64
+    dl $ver linux arm64
+    printf "    %s:\n" darwin
+    dl $ver darwin amd64
+    printf "    %s:\n" windows
+    dl $ver windows amd64 .exe
+}
 
-
+dlver ${1:-v1.16.2}
