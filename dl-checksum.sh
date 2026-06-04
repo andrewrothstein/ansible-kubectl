511edb850d205894289e46c0bd981a3cc5358d54
Andrew Rothstein <andrew.rothstein@gmail.com>
2026-06-04 11:42:02 -0400

    kubectl:1.36.1

--- dl-checksum.sh
+++ dl-checksum.sh
@@ -38,7 +38,7 @@ dlver () {
     dl $ver windows arm64 .exe
 }
 
-#dlver v1.32.12
-#dlver v1.33.8
-#dlver v1.34.4
-dlver ${1:-v1.35.4}
+#dlver v1.33.12
+#dlver v1.34.8
+#dlver v1.35.5
+dlver ${1:-v1.36.1}
